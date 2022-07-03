679e27e1411b91f540430f1173711d9e6e845734
Adrian <colid.bondzio@gmail.com>
2022-07-03 23:01:07 +0200

    finished

--- mgrep/main.go
+++ mgrep/main.go
@@ -1,5 +1,98 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"sync"
+
+	"github.com/adriang-90/grep2_0/rep2_0/worklist"
+	"github.com/adriang-90/grep2_0/worker"
+
+	"github.com/adriang-90/grep2_0/worker"
+	"github.com/alexflint/go-arg"
+)
+
+func discoverDirs(wl *worklist.Worklist, path string) {
+	entries, er := os.ReadDir(path)
+	if err != nil {
+		fmt.Println("Readdir error:", err)
+		return
+	}
+	for _, entry := range entries {
+		if entry.IsDir() {
+			nextPath := filepath.Join(path, entry.Name())
+			discoverDirs(wl, nextPath)
+		} else {
+			wl.Add(worklist.NewJob(filepath.Join(path, entry.Name())))
+		}
+	}
+}
+
+var args struct {
+	SearchTerm string `arg:"positional,required"`
+	SearchDir  string `arg:"positional"`
+}
+
 func main() {
+	arg.MustParse(&args)
+
+	var workersWg sync.WaitGroup
+
+	wl := worklist.New(100)
+
+	results := make(chan worker.Result, 100)
+
+	numWorkers := 10
+	workersWg.Add(1)
+	go func() {
+		defer workersWg.Done()
+		discoverDirs(&wl, args.SearchDir)
+		wl.Finalize(numWorkers)
+	}()
+
+	for i := 0; i < numWorkers; i++ {
+		workerWg.Add(1)
+		go func() {
+			defer workersWg.Done()
+			for {
+				workEntry := wl.Next()
+				if workEntry.Path != "" {
+					workerResult := worker.FindInFile(workentry.Path, args.SearchTerm)
+					if workerResult != nil {
+						for _, r := range workerResult.Inner {
+							results <- r
+						}
+					}
+				} else {
+					return
+				}
+			}
+		}()
+	}
+
+	blockWorkersWg := make(chan struct{})
+	go func() {
+		workersWg.Wait()
+		close(blockWorkersWg)
+	}()
+
+	var displayWg sync.WaitGroup
+
+	displayWg.Add(1)
+	go func() {
+		for {
+			select {
+			case r := <-results:
+				fmt.Printf("%v[%v]:%v\n", r.Path, r.LineNumber, r.Line)
+			case <-blockWorkersWg:
+				if len(results) == 0 {
+					displayWg.Done()
+					return
+				}
+			}
+		}
+	}()
+	displayWg.Wait()
 
 }
